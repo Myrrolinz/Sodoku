6afd53f61f4bdb6469b918734f53cfab3129ffaf
@civilizwa <2243987073@qq.com>
2023-06-26 11:25:47 +0800

    定义生成器

--- shudu/InputHandler.h
+++ shudu/InputHandler.h
@@ -1,15 +1,15 @@
 #pragma once
 #include <iostream>
 #include <string>
-
+#include "GenerateHandler.h"
 using namespace std;
 
 class InputHandler {
 public:
 	void check(int argc, char** argv);
 	void getFinal(int num);
-	void getQues(int num);
-
+	void getQues(int num,int begin_num,int end_num);
+	Generatehandler generator;//定义数独题目生成器
 private:
 	int isNum(const string& s);
 };
